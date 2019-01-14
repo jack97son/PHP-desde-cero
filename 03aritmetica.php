3ffb7b7f9f32e9f085fdfaca7840bbb9846d126c
Jackson <jackson.parada@globant.com>
2019-01-14 15:50:30 -0500

    primeros pasos php

--- 03aritmetica.php
+++ 03aritmetica.php
@@ -1,2 +1,29 @@
 <?php
 
+/**
+ *  Operadores aritmeticos
+ *  + suma
+ *  - resta
+ *  * multiplicacion
+ *  / division
+ *  % modulo
+ */
+
+//  echo 3 + 3; // suma
+// echo 6 - 2; // resta
+// var_dump(6 -2 + 5);
+
+// var_dump( (4 + 3) * 2 / 10);
+
+/**
+ *  Prioridad de los operadores aritmeticos
+ *  Multiplicacion
+ *  Division
+ *  Modulos
+ *  Suma
+ *  Resta
+ */
+
+// var_dump (7 % 3);
+
+var_dump(2 + 4 - 2 * 4 / 2 );
---
+++ 04variables.php
@@ -0,0 +1,57 @@
+<?php
+
+// asigna el # 3 a la variable tres
+// $tres = 3;
+
+// var_dump($tres);
+
+// $pi = 3.1416;
+// var_dump($pi);
+
+
+// var_dump($pi * 5);
+
+/**
+ *  $nombre = 'Luis';
+ *  $Nombre = 'Carlos';
+ */
+    
+
+// $nombre = 'Luis';
+// var_dump($nombre);
+
+// $Nombre = 'Carlos';
+// var_dump($Nombre);
+
+// boolean
+// false or true
+
+// $cosaFalsa = false;
+// var_dump($cosaFalsa);
+
+// $cosaVerdadera = true;
+// var_dump($cosaVerdadera);
+
+// arrays
+// $miPrimerArray = [1, 2, 3];
+// $otroArray = array(5, 6, 7, 8);
+// var_dump($miPrimerArray);
+// var_dump($otroArray);
+
+// asignacion especial
+
+// $polos = 2;
+// $polos = $polos + 1;
+
+// $polos += 8;
+
+// $polos--;
+// var_dump($polos);
+
+/** 
+ *  $lowerCamelCase
+ * 
+ * 
+*/
+
+$nombreCompletoDeAlguien = 'Waldo Araya';
