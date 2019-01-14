76e9685d71bb055ab13b5ca6f29e4770eb2ba2c6
Jackson <jackson.parada@globant.com>
2019-01-14 10:22:08 -0500

    primeros archivos php

---
+++ 02comentarios.php
@@ -0,0 +1,24 @@
+<?php
+
+// Comentario de linea
+// echo 'Hola mundo';
+
+/*
+
+Comentario de varias lineas
+
+*/
+
+/* 
+* Comentario
+* de varias lineas
+* mas bonito/legible
+* 
+ */
+
+ /**
+  * Doc blocks
+  * Descripcion de una variable
+  * @var int
+  */
+  $edad = 25;
